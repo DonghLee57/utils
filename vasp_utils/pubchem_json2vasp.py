11ba0f87c2c4c4a32be8b7fc293806e0b38b0c02
Dongheon <zwqa1234@gmail.com>
2024-06-27 20:30:33 +0900

    Create pubchem_json2vasp.py

---
+++ vasp_utils/pubchem_json2vasp.py
@@ -0,0 +1,22 @@
+import sys
+import json
+import numpy as np
+from ase import Atoms
+from ase.io import  write
+from ase.data import chemical_symbols
+
+tmp = json.load(open(sys.argv[1],'r'))
+lattice = float(sys.argv[2])
+
+out = Atoms(pbc=[1,1,1])
+out.cell = np.eye(3)*lattice
+elem = tmp['PC_Compounds'][0]['atoms']['element']
+positions = np.array([tmp['PC_Compounds'][0]['coords'][0]['conformers'][0]['x'],
+                      tmp['PC_Compounds'][0]['coords'][0]['conformers'][0]['y'],
+                      tmp['PC_Compounds'][0]['coords'][0]['conformers'][0]['z']]).T
+
+for i in range(len(elem)):
+    out.append(chemical_symbols[elem[i]])
+    out.positions[-1] = positions[i]
+
+write('POSCAR_converted.vasp', images=out, format='vasp')
